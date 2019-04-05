d2b2188493e9bbe57ac44409c5ae0753ef139aa1
Karl <karl.devooght@gmail.com>
2019-04-05 22:47:48 +0200

    step-2

---
+++ src/heroes/heroes.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post, Delete, Body, Param, HttpCode, Header } from '@nestjs/common';
+import { create } from 'domain';
+
+/**
+ * Rest APi related to super-heroes
+ */
+@Controller('heroes')
+export class HeroesController {
+
+    @Post()
+    @Header('Cache-Control', 'none')
+    @HttpCode(200)
+    create(@Body() hero: string): string {
+        return 'New Hero';
+    }
+    
+    @Get(':id')
+    read(@Param('id') id: string): string {
+        return 'Batman';
+    }
+
+    @Post()
+    update(@Body() hero: string): string {
+        return "Updated Batman"
+    }
+
+    @Delete(':id')
+    delete() {
+        return "Deleted hero"
+    }
+
+    @Get()
+    findAll(): string[] {
+        return [ 'Batman' , 'Superman' ];
+    }
+
+    
+}
